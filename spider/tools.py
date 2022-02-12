9667f68f2b2577e246c988e2fa94d0f9128a1048
jinsong <jinsong@58.com>
2022-02-13 01:02:34 +0800

    1

--- spider/tools.py
+++ spider/tools.py
@@ -6,22 +6,36 @@ from lxml import etree
 from conf.proxyIp import getIp
 import time
 
-def getProxyList():
+
+def getProxyRes():
     headers = {
         'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'}
     url = "http://webapi.http.zhimacangku.com/getip?num=400&type=1&pro=&city=0&yys=0&port=1&pack=214550&ts=0&ys=0&cs=0&lb=1&sb=0&pb=5&mr=1&regions="
     req = Request(url=url, headers=headers)
-    response = urlopen(req)
+    response = urlopen(req).read().decode()
+
+    return response
+
+
+def getProxyObjectList():
+    proxyStr = getProxyRes()
+    proxyList = proxyStr.split('\r\n')
+    result = []
+    proxy = {
+        'http': 'http://59.34.123.44:54213'
+    }
+    for proxyItem in proxyList:
+        temp = {'http': 'http://' + proxyItem}
+        result.append(temp)
+    print(result)
 
 
 def requesttool(url):
-
-
-    #http://webapi.http.zhimacangku.com/getip?num=400&type=1&pro=&city=0&yys=0&port=1&pack=214550&ts=0&ys=0&cs=0&lb=1&sb=0&pb=5&mr=1&regions="
+    # http://webapi.http.zhimacangku.com/getip?num=400&type=1&pro=&city=0&yys=0&port=1&pack=214550&ts=0&ys=0&cs=0&lb=1&sb=0&pb=5&mr=1&regions="
     headers = {
         'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'}
 
-    #proxy = urllib3.ProxyManager(proxy_url="http://59.34.123.44:54213", proxy_headers=headers)
+    # proxy = urllib3.ProxyManager(proxy_url="http://59.34.123.44:54213", proxy_headers=headers)
 
     proxy = {
         'http': 'http://59.34.123.44:54213'
@@ -51,7 +65,6 @@ def matchKeyword(str):
 
 
 def getss(url, module_xpath, sign_xpath):
-
     result = []
 
     article_name = ""
@@ -70,17 +83,17 @@ def getss(url, module_xpath, sign_xpath):
                 # add: 判断 article_url 是相对路径还是绝对路径
                 if article_url[0:4] != "http":
 
-                    if(article_url[0]!="/"):
+                    if (article_url[0] != "/"):
                         article_url = url + article_url
                     else:
                         a = url.split('//')[0]
                         b = url.split("//")[1].split('/')[0]
-                        article_url = a+"//"+b + article_url
+                        article_url = a + "//" + b + article_url
                 # 获取新闻时间
 
                 # time.sleep(40)
                 sign_text = "-"
-                if sign_xpath!= "":
+                if sign_xpath != "":
                     sign_ = searchEle(requesttool(article_url), sign_xpath)
 
                     if len(sign_) > 0:
